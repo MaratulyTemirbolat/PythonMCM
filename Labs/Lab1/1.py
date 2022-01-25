4a35022bb70a9c37dc21ea84b49a9f08ffc0e8a3
MaratulyTemirbolat <47031729+MaratulyTemirbolat@users.noreply.github.com>
2022-01-25 22:36:59 +0600

    Delete Labs directory

--- Labs/Lab1/1.py
+++
@@ -1,3 +0,0 @@
-a = 4
-b = 9
-print('a is {} b is {}'.format(a,b))
--- Labs/Lab10/Lab10_2_Maratuly_Temirbolat.py
+++
@@ -1,7 +0,0 @@
-def calculateSum(number):
-	myList = [number+1 for number in range(number)]
-	return sum(myList)
-
-number = int(input('Please,type the number of n: '))
-total = calculateSum(number)	
-print(total)
